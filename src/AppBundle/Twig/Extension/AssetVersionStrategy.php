90d6c0c99783c3d379e273648696648373046861
Michael Cenker <michael.cenker@k2ol.cz>
2017-05-15 14:48:18 +0200

    use custom asset version strategy

---
+++ src/AppBundle/Twig/Extension/AssetVersionStrategy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+
+class AssetVersionStrategy implements VersionStrategyInterface
+{
+    /**
+     * @var string
+     */
+    private $root;
+
+    /**
+     * @param string $root
+     */
+    public function __construct($root)
+    {
+        $this->root = $root;
+    }
+
+    /**
+     * Should return the version by the given path (starts from public root)
+     *
+     * @param string $path
+     * @return string
+     */
+    public function getVersion($path)
+    {
+        $fullPath = $this->root . '/../web/' . $path;
+        if (file_exists($fullPath)) {
+            $version = filemtime($fullPath);
+            if (!$version) {
+                $version = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+            }
+
+            return $version;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Determines how to apply version into the given path
+     *
+     * @param string $path
+     * @return string
+     */
+    public function applyVersion($path)
+    {
+        $version = $this->getVersion($path);
+        return $version ? sprintf('%s?v=%s', $path, $this->getVersion($path)) : $path;
+    }
+}
