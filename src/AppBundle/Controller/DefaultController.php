e5bd1f24ac4d87146380c5659e03e6a42e41a519
Michael Cenker <Mike.Cenker@gmail.com>
2018-01-20 08:27:53 +0100

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use AppBundle\Entity\KingsRide;
 
 class DefaultController extends Controller
 {
-    protected const CURRENT_YEAR = 2017;
+    protected const CURRENT_YEAR = 2018;
 
     /**
      * @Route("/{year}", name="homepage", requirements={"year"="\d+"})
