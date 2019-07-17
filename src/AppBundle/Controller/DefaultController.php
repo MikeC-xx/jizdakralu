c6d87c012f964bc18f94c58bc479f451a8bfdea5
Mikec <>
2019-07-17 09:27:20 +0200

    add new photos

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -162,6 +162,11 @@ class DefaultController extends Controller
                 'cover' => 'https://lh3.googleusercontent.com/qf7zq4TjeNniGXUpkCCRhz7MxXJ5en22Y4mvfAxG0ad394TPrAyXHYCWY1OH84Pf1kxFEjpH_H7E_RXnxMnJCtpYUiGdknCXgOhOTFYVAn2YiYkAz3G12BRVgwVdyTqw97GaS8uYzEVui8BHDUjQobdW9PCNgBb-SMZjHu1R-UzGjBINteUgBhS43T_ehEdUi-MsJGr71110Bof-babSi57O0SdIvR_prdwFq1zrW7qpKMX1iO7Ci6ngRR0HkKmt33CYwyXxLuk-HTJbDU8hcY7HyAbyq4kzzYTj-g4RzUi0xBjsRxuP_PSS_RN6vwFhjzbYkIoPMthCD4cCSysSQbSWyB_pTlduVmVjGOZl-g2n0O8i-aFYQc_GMNbCegADCSbm67iUXeihC-Jnp6MFguLBc2sHG37Z9uI8Yo7EnnyjTYRDehLZU8FOmvvOmGBHmObs_zxR5A2moeF2DjwMb5lNPvE4nwMYLZfuliwCzIlKcQzYgz2t_d44gWN9FEmrgifEmQtoxFRo896ZU0os3fLuz7I3OR94KgDIM4rZV12ysPw_uUQXjzDOLgUc-sLVKsoWX-aii9fdq8mFPXavLnIbIOZW6kD6OtwU9KzTl92NT0UwenSvL59FY8oDfDNMEW6wiLJJEpHMVSq_rXkwYjIf-TJKaL2F',
                 'year' => 2018,
                 'url' => 'https://photos.app.goo.gl/YyVZyfgkswDtTVHy5'
+            ),
+            array(
+                'cover' => 'https://lh3.googleusercontent.com/1NMXkHIQu0d_El14SGbIHhHsqScm7L5yjD5MPBD9wFCgk5IwU8qHK_Rrl6T0ls0n7SRuJ7w_qXkr3xyxwr2E8oonkKZCSudR4ri-k4RtPXxq3sha88GQJGfFk5f5MwK6KtdDaI5k1XkrceNkMcn5qPbnzHvI5KmJllnzdmsAC1imSVh5SQaVKvQ8PJXnM-idwmkrmemOehsLLNF-1SxKoTVpuKRCMBqyZpgmvPKpfC4fR1tr-UBUm47DUbwnvR7GvR-zsn-QpNXvZqZWer1AsX1znXUmaDmbIsJdb0zA1gvrz4Wqdc1DszulBmzyiKTOX4U8gZCbnWu5JcisUP4rRsI4x5MrUPD2MgZZOfel2tXHW2yfIGU10I_2Mwhq97jnPbFNDqzHcLsamhyNUrPE9qOjEuC9YvqVx2VRz7L1tar1gepvZ9lvAPIM4rLD23W0HhCiYMH9F_mkh15bRvjS1QrS8cROabr7zbsxhD2kL0rITWqWiTjROxj7lhqOBViR_siI5Y8c36f6UbV4qM_rRMMmA0U9AuKEaPafOHNPc2TgWEa476jItDiSfPF7Ni1HribD0juapCm8GNVjeZhih-4fFzO8eo26m19jfzqMRVhGUdt58IW9FmXH2Ji1EdEN4E25nWg0SLzImizwblLXxoybqe7cjQTDmH3_okNh77SHf5ZxsFIARWhr4fgvtEILYmNR0ymOoQ1crz-zdXGMYdd-=w1920-h1275-no',
+                'year' => 2019,
+                'url' => 'https://photos.app.goo.gl/ue2PAaDrzbgGhCnR9'
             )
         );
 
