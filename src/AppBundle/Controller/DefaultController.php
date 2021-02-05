ceeaa9252d38afb98ccd921390cc61e4448baa4f
Mikec <>
2021-02-05 19:23:57 +0100

    update

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use AppBundle\Entity\KingsRide;
 
 class DefaultController extends Controller
 {
-    protected const CURRENT_YEAR = 2020;
+    protected const CURRENT_YEAR = 2021;
 
     /**
      * @Route("/{year}", name="homepage", requirements={"year"="\d+"})
